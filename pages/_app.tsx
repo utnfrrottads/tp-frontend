8f9b47d8e73af5a11e50ec10492670f56cb05562
Alejandro Navarini <anavarini@frro.utn.edu.ar>
2022-09-08 17:01:32 -0300

    Installed mui and formik"

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,27 @@
+/* eslint-disable import/extensions */
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { CacheProvider, ThemeProvider } from '@emotion/react'
+import { CssBaseline } from '@mui/material'
+import lightTheme from '../styles/theme/lightTheme'
+import createEmotionCache from '../utility/createEmotionCache'
+
+const clientSideEmotionCache = createEmotionCache()
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   const [queryClient] = React.useState(() => new QueryClient())
+  const emotionCache = clientSideEmotionCache
   return (
     <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
+      <CacheProvider value={emotionCache}>
+        <ThemeProvider theme={lightTheme}>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </CacheProvider>
     </QueryClientProvider>
   )
 }
