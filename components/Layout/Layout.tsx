9c9db855dbfd662168655b16ebb246fb8261c347
AlanHoltz <alanholtz1999@gmail.com>
2022-09-09 23:42:40 -0300

    Responsive Menu for Navbar

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,19 +1,41 @@
-import React, { FunctionComponent } from 'react';
+import React, { useState } from 'react';
 import Navbar from '../Navbar/Navbar';
 import styles from './Layout.module.css';
+import Button from '@mui/material/Button';
+import { CgLogIn } from 'react-icons/cg';
 
-type LayoutProps = {
+interface LayoutProps {
     children: React.ReactNode
 }
 
-const Layout: FunctionComponent<LayoutProps> = ({ children }) => {
+const Layout = ({ children }: LayoutProps) => {
+
+
+    const [menuIsOpen, setMenuIsOpen] = useState(false);
+
+
     return (
+
         <main>
-            <Navbar/>
+            <Navbar auth={null} setMenuIsOpen={setMenuIsOpen} menuIsOpen={menuIsOpen} />
             <section className={styles.body}>
                 {children}
             </section>
+            <div style={{ height: menuIsOpen ? "calc(100% - 64px)" : "0" }} className={styles.menu}>
+                <Button sx={{
+                    background: "#FFFFFF",
+                    color: "#F76776",
+                    border: "none",
+                    "&:hover": {
+                        background: "#DADADA"
+                    }
+                }}
+                    variant="contained" size="small" startIcon={<CgLogIn />}>
+                    Ingresar
+                </Button>
+            </div>
         </main>
+
     )
 };
 
--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,24 +1,57 @@
-import React, { FunctionComponent } from 'react';
+import React from 'react';
 import styles from './Navbar.module.css';
 import Avatar from '@mui/material/Avatar';
-import {VscTriangleDown} from 'react-icons/vsc';
+import { VscTriangleDown } from 'react-icons/vsc';
+import Button from '@mui/material/Button';
+import { CgLogIn } from 'react-icons/cg';
+import Hamburger from 'hamburger-react';
+import useMediaQuery from '@mui/material/useMediaQuery';
 
-type NavbarProps = {
-    auth?: object,
+type Auth = {
+    user_id: string,
+    user_name: string,
+    user_email: string,
+
 }
 
-const Navbar: FunctionComponent<NavbarProps> = ({ auth }) => {
+interface NavbarProps {
+    auth: Auth | null,
+    setMenuIsOpen: React.Dispatch<React.SetStateAction<boolean>>,
+    menuIsOpen: boolean,
+}
+
+const Navbar = ({ auth, setMenuIsOpen, menuIsOpen }: NavbarProps) => {
+
+    const isResponsive: boolean = useMediaQuery("(max-width:768px)");
 
     return (
         <section className={styles.navbar}>
             <h1>LOGO</h1>
-            <div>
-                <div className={styles.avatar}>
-                    <Avatar sx={{ bgcolor: "#FFFFFF", color: "#F76776", width: 32, height: 32, fontSize:"0.8rem" }} alt="avatar">NA</Avatar>
-                    <span>Nombre y Apellido</span>
-                    <VscTriangleDown size={15}/>
-                </div>
-            </div>
+
+            {isResponsive && <Hamburger toggled={menuIsOpen} onToggle={(toggle) => setMenuIsOpen(!menuIsOpen)} />}
+
+            {!isResponsive && <React.Fragment>
+                {auth ?
+                    <div className={styles.avatar}>
+                        <Avatar sx={{ bgcolor: "#FFFFFF", color: "#F76776", width: 32, height: 32, fontSize: "0.8rem" }} alt="avatar">NA</Avatar>
+                        <span>Nombre y Apellido</span>
+                        <VscTriangleDown size={15} />
+                    </div>
+                    :
+                    <Button sx={{
+                        background: "#FFFFFF",
+                        color: "#F76776",
+                        border: "none",
+                        "&:hover": {
+                            background: "#DADADA"
+                        }
+                    }}
+                        variant="contained" size="small" startIcon={<CgLogIn />}>
+                        Ingresar
+                    </Button>}
+
+            </React.Fragment>}
+
         </section>
     )
 };
