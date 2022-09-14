958b1c916ae5e18172a753d5ff55fceda0872e5a
luca braccani <lucab_2001@hotmail.com>
2022-09-14 10:48:03 -0300

    Arreglo bugs del login

--- pages/login.js
+++ pages/login.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import {useUser} from '../context/userContext'
 import { useRouter } from 'next/router'
 import FormLogin from "../components/Login/FormLogin"
+import Link from "next/link";
 
 const endpoint = "http://localhost:9000/api/v1/users/"
 
@@ -60,7 +61,7 @@ export default function Login(props){
 
 
       setUser(loggedUser)
-      router.push('/')
+      router.push('/chat')
 
   }
 
@@ -69,9 +70,16 @@ export default function Login(props){
     return(
         <div className="relative flex flex-col justify-center items-center h-screen w-screen bg-gradient-to-b from-white to-black">
 
-            <div className="flex flex-col items-center justify-center bg-gray-300 w-[400px] h-[300px] rounded-2xl relative shadow-slate-800 shadow-lg ">
-            <svg className="shadow-md absolute top-[-20px] bg-slate-800 rounded-full w-32 h-32 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+          <div className="flex flex-col items-center justify-center bg-gray-300 w-[400px] h-[300px] rounded-2xl relative shadow-slate-800 shadow-lg ">
+          <div className="absolute top-0 left-0 cursor-pointer">
+          <Link href='/'>
+            <svg className="h-10 w-10 text-black" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
+              <path stroke-linecap="round" strokeLinejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
+            </svg>
+          </Link>
+          </div>
+            <svg className="shadow-md absolute top-[-20px] bg-slate-800 rounded-full w-32 h-32 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
+                <path stroke-linecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
             </svg>   
             <FormLogin
                 handleSubmit={handleSubmit}
@@ -79,9 +87,10 @@ export default function Login(props){
                 username={usernameForm}
                 password={passwordForm}
             />
-            </div>
+          </div>
 
         </div>
     )
 
 }
+
