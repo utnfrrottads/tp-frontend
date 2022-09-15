8005704e738a3a66a38d19b7971558d0e516471b
Oscar Dorronzoro <oscar.dorronzoro@gmai.com>
2022-09-15 19:38:17 -0300

    article from list redirect to article's detail

--- src/app/articles/articles-list/articles-list.component.ts
+++ src/app/articles/articles-list/articles-list.component.ts
@@ -1,15 +1,26 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { IArticle } from 'src/app/entities/article';
+import { ArticleService } from 'src/app/services/article.service';
 
 @Component({
   selector: 'app-articles-list',
   templateUrl: './articles-list.component.html',
   styleUrls: ['./articles-list.component.scss'],
 })
-export class ArticlesListComponent {
+export class ArticlesListComponent implements OnInit{
 
   @Input() articles: IArticle[] = [];
   gridView = false;
+
+  constructor(
+    private articleService: ArticleService,
+  ) { }
+
+  ngOnInit(): void {
+    if (this.articles.length === 0) {
+      this.articles = this.articleService.getArticles();
+    }
+  }
 
   toggleGridView(e: boolean): void {
     this.gridView = e;
